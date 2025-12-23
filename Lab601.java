288cf2255f2e3edc0f3a2b6c595c30291aa59d6f
Mick13176 <th.thitivat_st@tni.ac.th>
2025-12-23 13:17:21 +0700

    Add files via upload

---
+++ Lab601.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Lab601 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[5];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter number " + (i + 1) + ": ");
+            numbers[i] = sc.nextInt();
+
+            while (numbers[i] <= 0) {
+                System.out.print("The number must be positive! Enter number again: ");
+                numbers[i] = sc.nextInt();
+            }
+        }
+
+        System.out.print("\nList of even number: ");
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                System.out.print(numbers[i] + " ");
+            }
+        }
+
+        System.out.print("\nList of odd number: ");
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                System.out.print(numbers[i] + " ");
+            }
+        }
+    }
+}
